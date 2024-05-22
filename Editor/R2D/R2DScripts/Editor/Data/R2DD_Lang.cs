49ebf7e9b9f9419c19466052b5bf94e21cfdcef3
MrBaoquan <mrma617@gmail.com>
2024-05-22 11:09:47 +0800

    add UIPreview && R2D

---
+++ Editor/R2D/R2DScripts/Editor/Data/R2DD_Lang.cs
@@ -0,0 +1,119 @@
+﻿//----------------------------------------------
+// Ruler 2D
+// 版权所有 © 2015-2020 Pixel Fire™
+//----------------------------------------------
+
+namespace R2D
+{
+    public static class R2DD_Lang
+    {
+        public const string menuAccess = "Window/Ruler 2D/打开 %#=";
+        public const string toggleGuides = "Window/Ruler 2D/切换标线显示 %;";
+        public const string toggleGuidesLock = "Window/Ruler 2D/切换标线锁定 %#;";
+        public const string toggleGrid = "Window/Ruler 2D/切换网格 %'";
+        public const string windowTitle = "Ruler 2D";
+        public const string editorScene = "Unity 2D";
+        public const string worldUnits = "环境单位";
+        public const string pixels = "像素";
+        public const string context = "环境";
+        public const string unitType = "单位类型";
+        public const string refPixels = "参考像素";
+        public const string referencePixels = "每单位参考像素";
+        public const string displaySelectedCoords = "展示选中坐标";
+        public const string preferColliders = "偏好碰撞体";
+        public const string displayGuides = "展示标线";
+        public const string lockGuides = "锁定标线";
+        public const string displayGuideCoords = "展示鼠标悬停坐标";
+        public const string snapGuideToInt = "标线吸附至整数";
+        public const string presets = "预设";
+        public const string clearGuides = "清除所有标线";
+        public const string titleGuides = " 标线";
+        public const string titleRulerSettings = " 设置";
+        public const string titleToolbox = " 工具箱";
+        public const string titleGrid = " 网格";
+        public const string guide = "引导";
+        public const string guideHelp = "从场景中的标尺拖动来创建标线，并拖动它们来移除。";
+        public const string nguiInvalidContext = "要与 NGUI 小部件配合使用，您必须从设置面板将环境更改为 UI 根。";
+        public const string canvasInvalidContext = "要与 Canvas 对象配合使用，您必须从设置面板将环境更改为 Canvas。";
+        public const string nguiError = "NGUI 对齐不可用。请向开发者报告此问题。";
+        public const string undo = "撤销上一操作";
+        public const string lblAlign = "对齐";
+        public const string lblDistribute = "分布";
+        public const string lblSpace = "间隔";
+        public const string tipAlignTop = "顶边对齐";
+        public const string tipAlignHMid = "垂直中心对齐";
+        public const string tipAlignBot = "底边对齐";
+        public const string tipAlignLeft = "左边对齐";
+        public const string tipAlignVMid = "水平中心对齐";
+        public const string tipAlignRight = "右边对齐";
+        public const string tipDistTop = "顶边分布";
+        public const string tipDistYMid = "垂直中心分布";
+        public const string tipDistBot = "底边分布";
+        public const string tipDistLeft = "左边分布";
+        public const string tipDistXMid = "水平中心分布";
+        public const string tipDistRight = "右边分布";
+        public const string lblSnapToGuide = "吸附至标线";
+        public const string lblUseEdgesForSnap = "边缘吸附至标线";
+        public const string lblMeasure = "测量";
+        public const string tipSnapLeft = "向左吸附至最近的标线";
+        public const string tipSnapRight = "向右吸附至最近的标线";
+        public const string tipSnapTop = "向上吸附至最近的标线";
+        public const string tipSnapBot = "向下吸附至最近的标线";
+        public const string tipSpaceX = "从最小的X开始，使X方向上的对象间隔均匀。\n\n提示：为获得最佳结果，将您的对象按照期望的X序列放置。";
+        public const string tipSpaceY = "从最小的Y开始，使Y方向上的对象间隔均匀。\n\n提示：为获得最佳结果，将您的对象按照期望的Y序列放置。";
+        public const string tipMeasure = "激活测量工具。\n\n提示：在场景中拖动以自由测量。";
+        public const string tipMeasureObj = "选择两个对象并按下此按钮来测量它们之间的距离。";
+        public const string tipClearMeasure = "清除测量线";
+        public const string measureAlert = "测量工具准备就绪";
+        public const string gridX = "单位水平宽度";
+        public const string gridY = "单位垂直高度";
+        public const string gridOriginX = "原点 X";
+        public const string gridOriginY = "原点 Y";
+        public const string gridCols = "列数";
+        public const string gridRows = "行数";
+        public const string snapToGrid = "吸附至网格";
+        public const string gridEnabled = "显示网格";
+        public const string nguiGridHelp = "使用标准移动工具来使 NGUI 对象吸附至网格。";
+        public const string sceneModeError = "当场景视图处于 3D 模式时，Ruler 2D 不可用。";
+        public const string set2DSceneMode = "将场景视图切换到 2D 模式。";
+        public const string undoAlignTop = "顶边对齐";
+        public const string undoAlignVertical = "垂直中心对齐";
+        public const string undoAlignBottom = "底边对齐";
+        public const string undoAlignLeft = "左边对齐";
+        public const string undoAlignHorizontal = "水平中心对齐";
+        public const string undoAlignRight = "右边对齐";
+        public const string undoDistroTop = "顶边分布";
+        public const string undoDistroVertical = "垂直中心分布";
+        public const string undoDistroBottom = "底边分布";
+        public const string undoDistroLeft = "左边分布";
+        public const string undoDistroHorizontal = "水平中心分布";
+        public const string undoDistroRight = "右边分布";
+        public const string undoSpaceX = "水平间距";
+        public const string undoSpaceY = "垂直间距";
+        public const string undoSnapLeft = "向左吸附";
+        public const string undoSnapRight = "向右吸附";
+        public const string undoSnapUp = "向上吸附";
+        public const string undoSnapDown = "向下吸附";
+
+        // 层级上下文菜单项
+        public const string contextAlignTopEdges = "GameObject/Ruler 2D/顶边对齐";
+        public const string contextAlignVerticalCenters = "GameObject/Ruler 2D/垂直中心对齐";
+        public const string contextAlignBottomEdges = "GameObject/Ruler 2D/底边对齐";
+        public const string contextAlignLeftEdges = "GameObject/Ruler 2D/左边对齐";
+        public const string contextAlignHorizontalCenters = "GameObject/Ruler 2D/水平中心对齐";
+        public const string contextAlignRightEdges = "GameObject/Ruler 2D/右边对齐";
+        public const string contextDistributeTopEdges = "GameObject/Ruler 2D/顶边分布";
+        public const string contextDistributeVerticalCenters = "GameObject/Ruler 2D/垂直中心分布";
+        public const string contextDistributeBottomEdges = "GameObject/Ruler 2D/底边分布";
+        public const string contextDistributeLeftEdges = "GameObject/Ruler 2D/左边分布";
+        public const string contextDistributeHorizontalCenters = "GameObject/Ruler 2D/水平中心分布";
+        public const string contextDistributeRightEdges = "GameObject/Ruler 2D/右边分布";
+        public const string contextSnapToGuideLeft = "GameObject/Ruler 2D/向左吸附至引导线";
+        public const string contextSnapToGuideRight = "GameObject/Ruler 2D/向右吸附至引导线";
+        public const string contextSnapToGuideUp = "GameObject/Ruler 2D/向上吸附至引导线";
+        public const string contextSnapToGuideDown = "GameObject/Ruler 2D/向下吸附至引导线";
+
+        // 系统警告
+        public const string detectMonitorChange = "R2D 警告：检测到显示器更改。请重启 R2D 以避免未知的视觉效果。";
+    }
+}
