49fe12e1accd92fc744d695527113071df79577a
MrBaoquan <mrma617@gmail.com>
2024-09-20 13:51:58 +0800

    remove useless namespace.

--- Editor/Utils.cs
+++ Editor/Utils.cs
@@ -5,7 +5,6 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text.RegularExpressions;
-using Sirenix.Utilities;
 using Unity.EditorCoroutines.Editor;
 using UnityEditor;
 using UnityEditor.Animations;
